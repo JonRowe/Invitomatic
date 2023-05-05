02a70fa30292840a4c24e3de1e260d7b276620c3
Jon Rowe <hello@jonrowe.co.uk>
2023-05-05 10:04:41 +0100

    Alphabetically order content context

--- lib/invitomatic/content.ex
+++ lib/invitomatic/content.ex
@@ -9,6 +9,53 @@ defmodule Invitomatic.Content do
   alias Invitomatic.Repo
 
   @doc """
+  Returns an `%Ecto.Changeset{}` for tracking section changes.
+
+  ## Examples
+
+      iex> change_section(section)
+      %Ecto.Changeset{data: %Section{}}
+
+  """
+  def change_section(%Section{} = section, attrs \\ %{}) do
+    Section.changeset(section, attrs)
+  end
+
+  @doc """
+  Creates a section.
+
+  ## Examples
+
+      iex> create_section(%{field: value})
+      {:ok, %Section{}}
+
+      iex> create_section(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_section(attrs \\ %{}) do
+    %Section{}
+    |> Section.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Deletes a section.
+
+  ## Examples
+
+      iex> delete_section(section)
+      {:ok, %Section{}}
+
+      iex> delete_section(section)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_section(%Section{} = section) do
+    Repo.delete(section)
+  end
+
+  @doc """
   Gets a single section.
 
   Raises `Ecto.NoResultsError` if the Section does not exist.
@@ -38,24 +85,6 @@ defmodule Invitomatic.Content do
   end
 
   @doc """
-  Creates a section.
-
-  ## Examples
-
-      iex> create_section(%{field: value})
-      {:ok, %Section{}}
-
-      iex> create_section(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_section(attrs \\ %{}) do
-    %Section{}
-    |> Section.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
   Updates a section.
 
   ## Examples
@@ -71,34 +100,5 @@ defmodule Invitomatic.Content do
     section
     |> Section.changeset(attrs)
     |> Repo.update()
-  end
-
-  @doc """
-  Deletes a section.
-
-  ## Examples
-
-      iex> delete_section(section)
-      {:ok, %Section{}}
-
-      iex> delete_section(section)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_section(%Section{} = section) do
-    Repo.delete(section)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking section changes.
-
-  ## Examples
-
-      iex> change_section(section)
-      %Ecto.Changeset{data: %Section{}}
-
-  """
-  def change_section(%Section{} = section, attrs \\ %{}) do
-    Section.changeset(section, attrs)
   end
 end
